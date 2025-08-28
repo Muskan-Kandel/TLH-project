9f264bb0af9a86349a361383c06c39b45d0cdd07
Muskan Kandel <muskankandel06@gmail.com>
2025-08-28 16:02:42 +0545

    projecttt

--- main.js
+++ main.js
@@ -17,21 +17,19 @@ modeSelect.addEventListener("dblclick", ()=>{
 }
 );
 
-// FOR ACCORDION
+//FOR ACCORDION 
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-   
     this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
-    }
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
   });
 }
+
